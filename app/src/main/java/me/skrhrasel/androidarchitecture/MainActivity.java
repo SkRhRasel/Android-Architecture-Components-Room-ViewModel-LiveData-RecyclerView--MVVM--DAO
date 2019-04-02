c05ee4bdf7d12e0d4d8d89ca023c14792ee4a3b0
SkRhRasel <sk.rasel.cse@gmail.com>
2019-04-03 04:13:39 +0600

    Creating Viewmodel class p5

--- app/src/main/java/me/skrhrasel/androidarchitecture/MainActivity.java
+++ app/src/main/java/me/skrhrasel/androidarchitecture/MainActivity.java
@@ -2,12 +2,31 @@ package me.skrhrasel.androidarchitecture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+                //.get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
